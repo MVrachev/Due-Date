c66f6014944b7d3ad5e33c5f4d32ff7544cbd95f
Martin Vrachev <martin.vrachev@gmail.com>
2019-01-16 17:28:25 +0200

    Add enums as a status of a task (#5)
    I replaced the status integer attribute with status enum attribute.
    
    I had to do some fixes in the tests also.
    
    Signed-off-by: Martin Vrachev <martin.vrachev@gmail.com>

--- components/elem.go
+++ components/elem.go
@@ -5,29 +5,56 @@ import (
 	"time"
 )
 
+type Status bool
+
+const (
+	IN_PROGRESS Status = false
+	DONE        Status = true
+)
+
+//status      int // Can be enums ??
+
 // Elem is object describing what an element in the table is
 type Elem struct {
 	date        time.Time
 	priority    int
 	description string
-	status      int // Can be enums ??
+	status      Status
 }
 
 // NewElem Creates a new Element
-func NewElem(date time.Time, priority int, description string, status int) Elem {
+func NewElem(date time.Time, priority int, description string) Elem {
 	return Elem{
 		date:        date,
 		priority:    priority,
 		description: description,
-		status:      status,
+		status:      IN_PROGRESS,
+	}
+}
+
+func (status Status) String() string {
+	if status == IN_PROGRESS {
+		return "IN PROGRESS"
+	} else {
+		return "DONE"
 	}
 }
 
 // Implements the String interface for the Elem object
 func (e Elem) String() string {
 	layout := "02 January	2006 15:04:05"
+
 	return fmt.Sprintf("Date: %v; 	priority: %v; description: %v; status: %v;",
-		fmt.Sprintf(e.date.Format(layout)), e.priority, e.description, e.status)
+		fmt.Sprintf(e.date.Format(layout)), e.priority, e.description, e.status.String())
+}
+
+// Equal Checks if the values of the right element are equal of those of the left element
+func Equal(right, left Elem) bool {
+	if right.date.Equal(left.date) && right.description == left.description &&
+		right.priority == left.priority && right.status == left.status {
+		return true
+	}
+	return false
 }
 
 // Equal Checks if the values of the right element are equal of those of the left element
