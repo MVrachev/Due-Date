c924a535812bb10fdf6e5a40595a66b63e00f0a0
Martin Vrachev <martin.vrachev@gmail.com>
2019-02-06 18:14:29 +0200

    Fix for tests (#12)
    I had problems in go test ./... and now I fix them.

--- data/data_handler.go
+++ data/data_handler.go
@@ -1,10 +1,12 @@
 package data
 
+import "github.com/end-date/user"
+
 // This object will be used to handle and encrypt all the data
 type DataHandler struct{}
 
 // Add a user as a record
-func (d *DataHandler) addIntoDatabase(user *User) {}
+func (d *DataHandler) addIntoDatabase(user *user.User) {}
 
 // Checks if a user is registered
 func (d *DataHandler) doesRecordExists() bool {
